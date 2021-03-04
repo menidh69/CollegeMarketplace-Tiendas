5198a0e6570ff8cf97f5710354b4050776174c0c
Victor <victor_chivas_013@hotmail.com>
2021-03-03 18:24:43 -0700

    Prueba edit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Saludos College Marketplace!</Text>
+      <Text>Saludos Terricolas!</Text>
       <Text>Aplicación para Tiendas</Text>
       <StatusBar style="auto" />
     </View>
