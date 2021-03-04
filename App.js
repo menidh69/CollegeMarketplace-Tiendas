8145b753df8772606c92604c1acbabb80d97301d
Victor <victor_chivas_013@hotmail.com>
2021-03-03 18:33:57 -0700

    Prueba Branch

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Saludos Terricolas!</Text>
+      <Text>Saludos Terricolas2!</Text>
       <Text>Aplicación para Tiendas</Text>
       <StatusBar style="auto" />
     </View>
